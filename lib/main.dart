28a4034dd09a3227f108164e562698113edc75d8
RubDaShen <142699406+RubDaShen@users.noreply.github.com>
2025-04-24 02:28:35 -0500

    feat: main entry point

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:mobile_application/app/home/home.dart';
+import 'package:mobile_application/app/menu/menu.dart';
+import 'package:mobile_application/hub/register/register.dart';
+
+import 'hub/login/login.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.pinkAccent),
+      ),
+      home: LoginPage(),
+      initialRoute: '/',
+      routes: {
+        '/register': (context) => RegisterPage(),
+        '/home': (context) => HomePage(),
+        '/menu': (context) => MenuPage(),
+      },
+    );
+  }
+}
