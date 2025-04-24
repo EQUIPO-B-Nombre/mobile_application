f8252e88d35e54ed315bbb5bde6d3ca8ccdc5a5f
paolomv02 <u202010039@upc.edu.pe>
2025-04-24 17:41:57 -0500

    feat: added patient model

---
+++ lib/app/patient_list/data/patient_model.dart
@@ -0,0 +1,35 @@
+class PatientModel {
+  final String? id;
+  final String? name;
+  final String dni;
+  final String? appointmentDate;
+  final String? telephone;
+
+  PatientModel({
+    this.id,
+    this.name,
+    required this.dni,
+    this.appointmentDate,
+    this.telephone,
+  });
+
+  factory PatientModel.fromJson(Map<String, dynamic> json) {
+    return PatientModel(
+      id: json['id'],
+      name: json['name'],
+      dni: json['dni'],
+      appointmentDate: json['appointmentDate'],
+      telephone: json['telephone'],
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'name': name,
+      'dni': dni,
+      'appointmentDate': appointmentDate,
+      'telephone': telephone,
+    };
+  }
+}
